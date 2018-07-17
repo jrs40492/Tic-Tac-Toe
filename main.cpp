9dcb4aa456f64eba6871e05125b2c99aec936a52
Jacob Swanson <jrs40492@gmail.com>
2018-07-16 22:46:08 -0400

    Refactor code

--- main.cpp
+++ main.cpp
@@ -7,231 +7,304 @@
 #include <string>
 using namespace std;
 
-bool winner = false;
-int **tic = new int *[3];
+int row = -1;
+int col = -1;
+string rowInput;
+string colInput;
 
-class moves //create class
+string winner;
+string tic[3][3];
+
+class TicTacToe
 {
 public:
-  int num;
-  int row;
-  int col;
   void create();
   void draw();
   void setvalues(int, int);
-  void win(); //create constructors
-  void taken()
+};
+
+int checkDigit(string input)
+{
+  if (isdigit(input[0]))
   {
-    while (tic[row][col] != 3)
-    {
-      cout << "Space taken, try again! "; //check if space is taken
-      cin >> row >> col;                  //get in new move
-    }
+    return stoi(input);
   }
-  void move()
+
+  return -1;
+}
+
+bool inRange(int val)
+{
+  return val > -1 && val < 3;
+}
+
+int checkMove(string input, string type)
+{
+  bool isValid = false;
+  int val = -1;
+
+  while (!isValid)
   {
-    if (num == 0 || num == 2 || num == 4 || num == 6 || num == 8) //check num
+    val = checkDigit(input);
+    if (val != -1 && inRange(val))
     {
-      tic[row][col] = 1; //set as 1 if player 1
-      num++;
+      isValid = true;
     }
     else
     {
-      tic[row][col] = 0; //set as 0 if player 2
-      num++;
+      cout << "Invalid value for " + type + ", pick again: ";
+      cin >> input;
     }
   }
-  ~moves()
-  {
-    delete[] tic; //destructor deletes tic
-  }
-};
 
-void moves::create()
+  return val;
+}
+
+bool spotTaken()
 {
-  for (int q = 0; q < 3; q++)
+  if (row == -1 || col == -1)
   {
-    tic[q] = new int[q]; //creates dynamic array
+    return true;
   }
+
+  if (tic[row][col] != " ")
+  {
+    return true;
+  }
+
+  return false;
+}
+
+void TicTacToe::create()
+{
   for (int k = 0; k < 3; k++)
   {
     for (int z = 0; z < 3; z++)
     {
-      tic[k][z] = 3; //sets each value in tic array to 3
+      tic[k][z] = " ";
     }
   }
 }
 
-void moves::draw()
+void draw()
 {
-  cout << "      0      1      2" << endl;
-  cout << "------------------------" << endl;
-  cout << "0     " << tic[0][0] << "      " << tic[0][1] << "      " << tic[0][2] << endl;
-  cout << "------------------------" << endl;
-  cout << "1     " << tic[1][0] << "      " << tic[1][1] << "      " << tic[1][2] << endl;
-  cout << "------------------------" << endl;
-  cout << "2     " << tic[2][0] << "      " << tic[2][1] << "      " << tic[2][2]; //draw the array each time a move is made
-}
+  string horizontalLine = "------------------------";
+  string spacing = "      ";
 
-void moves::win()
-{
-  if (tic[0][0] == 0 && tic[0][1] == 0 && tic[0][2] == 0) //checks each combination for a winner and breaks if found
+  cout << " " + spacing + "0" + spacing + "1" + spacing + "2" << endl;
+
+  // Dynamically draw tic tac toe board
+  for (int row = 0; row < 3; row++)
   {
-    draw();
-    cout << endl
-         << "Player 2 wins";
-    winner = 1;
-  }
-  else if (tic[0][0] == 1 && tic[0][1] == 1 && tic[0][2] == 1)
-  {
-    draw();
-    cout << endl
-         << "Player 1 wins";
-    winner = 1;
-  }
-  else if (tic[0][0] == 1 && tic[1][0] == 1 && tic[2][0] == 1)
-  {
-    draw();
-    cout << endl
-         << "Player 1 wins";
-    winner = 1;
-  }
-  else if (tic[0][0] == 0 && tic[1][0] == 0 && tic[2][0] == 0)
-  {
-    draw();
-    cout << endl
-         << "Player 2 wins";
-    winner = 1;
-  }
-  else if (tic[1][0] == 1 && tic[1][1] == 1 && tic[1][2] == 1)
-  {
-    draw();
-    cout << endl
-         << "Player 1 wins";
-    winner = 1;
-  }
-  else if (tic[1][0] == 0 && tic[1][1] == 0 && tic[1][2] == 0)
-  {
-    draw();
-    cout << endl
-         << "Player 2 wins";
-    winner = 1;
-  }
-  else if (tic[2][0] == 1 && tic[2][1] == 1 && tic[2][2] == 1)
-  {
-    draw();
-    cout << endl
-         << "Player 1 wins";
-    winner = 1;
-  }
-  else if (tic[2][0] == 0 && tic[2][1] == 0 && tic[2][2] == 0)
-  {
-    draw();
-    cout << endl
-         << "Player 2 wins";
-    winner = 1;
-  }
-  else if (tic[0][1] == 1 && tic[1][1] == 1 && tic[2][1] == 1)
-  {
-    draw();
-    cout << endl
-         << "Player 1 wins";
-    winner = 1;
-  }
-  else if (tic[0][1] == 0 && tic[1][1] == 0 && tic[2][1] == 0)
-  {
-    draw();
-    cout << endl
-         << "Player 2 wins";
-    winner = 1;
-  }
-  else if (tic[0][2] == 1 && tic[1][2] == 1 && tic[2][2] == 1)
-  {
-    draw();
-    cout << endl
-         << "Player 1 wins";
-    winner = 1;
-  }
-  else if (tic[0][2] == 0 && tic[1][2] == 0 && tic[2][2] == 0)
-  {
-    draw();
-    cout << endl
-         << "Player 2 wins";
-    winner = 1;
-  }
-  else if (tic[0][0] == 1 && tic[1][1] == 1 && tic[2][2] == 1)
-  {
-    draw();
-    cout << endl
-         << "Player 1 wins";
-    winner = 1;
-  }
-  else if (tic[0][0] == 0 && tic[1][1] == 0 && tic[2][2] == 0)
-  {
-    draw();
-    cout << endl
-         << "Player 2 wins";
-    winner = 1;
-  }
-  else if (tic[0][2] == 1 && tic[1][1] == 1 && tic[2][0] == 1)
-  {
-    draw();
-    cout << endl
-         << "Player 1 wins";
-    winner = 1;
-  }
-  else if (tic[0][2] == 0 && tic[1][1] == 0 && tic[2][0] == 0)
-  {
-    draw();
-    cout << endl
-         << "Player 2 wins";
-    winner = 1;
-  }
-  else if (num == 9)
-  {
-    cout << "Cats Game";
+    cout << horizontalLine << endl;
+    cout << std::to_string(row) + spacing;
+    for (int column = 0; column < 3; column++)
+    {
+      cout << tic[row][column] << spacing;
+    }
+    cout << endl;
   }
 }
 
-void moves::setvalues(int a, int b)
+void makeMove(int movesMade)
 {
-  row = a;
-  col = b; //sets user input to values row and col
+  if (movesMade % 2)
+  {
+    tic[row][col] = "O";
+  }
+  else
+  {
+    tic[row][col] = "X";
+  }
+
+  // Redraw board
+  draw();
+
+  // Reset values for next move
+  row = -1;
+  col = -1;
+}
+
+string checkWin(int movesMade)
+{
+  // Translate array to easy to read vars
+  string topLeft = tic[0][0];
+  string topCenter = tic[0][1];
+  string topRight = tic[0][2];
+
+  string midLeft = tic[1][0];
+  string midCenter = tic[1][1];
+  string midRight = tic[1][2];
+
+  string bottomLeft = tic[2][0];
+  string bottomCenter = tic[2][1];
+  string bottomRight = tic[2][2];
+
+  // Default comparator to X so only have to check for O
+  string comparator = "X";
+
+  // Can determine a win by first checking the 3 squares in any diagonal
+
+  // Get value of topLeft, set comparator to that value
+  if (topLeft.compare("X") != 0)
+  {
+    comparator = "O";
+  }
+
+  // Check straight line wins from topLeft. Diagonal wins will be checked from midCenter square
+
+  // check if topCenter matches topLeft
+  if (topCenter.compare(comparator) == 0)
+  {
+    // check if topRight matches topLeft
+    if (topRight.compare(comparator) == 0)
+    {
+      return comparator;
+    }
+  }
+
+  // check if midLeft matches topLeft
+  if (midLeft.compare(comparator) == 0)
+  {
+    // check if bottomLeft matches topLeft
+    if (bottomLeft.compare(comparator) == 0)
+    {
+      return comparator;
+    }
+  }
+
+  // Check squares that that can win from center square, straight lines and diagonals
+
+  comparator = "X";
+  // Get value of midCenter, set comparator to that value
+  if (midCenter.compare("X") != 0)
+  {
+    comparator = "O";
+  }
+
+  // check if topCenter matches midCenter
+  if (topCenter.compare(comparator) == 0)
+  {
+    // check if bottomCenter matches midCenter
+    if (bottomCenter.compare(comparator) == 0)
+    {
+      return comparator;
+    }
+  }
+
+  // check if midLeft matches midCenter
+  if (midLeft.compare(comparator) == 0)
+  {
+    // check if midRight matches midCenter
+    if (midRight.compare(comparator) == 0)
+    {
+      return comparator;
+    }
+  }
+
+  // check if topLeft matches midCenter
+  if (topLeft.compare(comparator) == 0)
+  {
+    // check if bottomRight matches midCenter
+    if (bottomRight.compare(comparator) == 0)
+    {
+      return comparator;
+    }
+  }
+
+  // check if topRight matches midCenter
+  if (topRight.compare(comparator) == 0)
+  {
+    // check if bottomLeft matches midCenter
+    if (bottomLeft.compare(comparator) == 0)
+    {
+      return comparator;
+    }
+  }
+
+  // Check straight line wins from bottomRight. Diagonal wins checked from midCenter square
+  comparator = "X";
+  if (bottomRight.compare("X") != 0)
+  {
+    comparator = "O";
+  }
+
+  // check if midRight matches bottomRight
+  if (midRight.compare(comparator) == 0)
+  {
+    // check if topRight matches bottomRight
+    if (topRight.compare(comparator) == 0)
+    {
+      return comparator;
+    }
+  }
+
+  // check if bottomCenter matches bottomRight
+  if (bottomCenter.compare(comparator) == 0)
+  {
+    // check if bottomLeft matches bottomRight
+    if (bottomLeft.compare(comparator) == 0)
+    {
+      return comparator;
+    }
+  }
+
+  if (movesMade == 9)
+  {
+    return "cat";
+  }
+
+  return "";
 }
 
 int main()
 {
-  char again = 'Y';
-  while (again == 'Y' || again == 'y')
+  string playAgain = "Y";
+  while (playAgain == "Y" || playAgain == "y")
   {
-    int ch1;
-    int ch2;
-    int num = 0;
-
-    moves play;
+    TicTacToe play;
     play.create();
-    cout << "X (1) goes first" << endl;
-    for (int i = 0; i < 9; i++)
+    cout << "X goes first" << endl;
+
+    // Draw board
+    draw();
+
+    // Only 9 moves can be made
+    for (int moves = 0; moves < 9; moves++)
     {
-      play.draw();
       cout << endl
-           << "Please enter a row and column: "; //ask for user input
-      cin >> ch1 >> ch2;
-      play.setvalues(ch1, ch2);
-      play.taken();
-      play.move(); //call needed constructors
-      num++;
-      if (num >= 5)
+           << "Enter row #: ";
+      cin >> rowInput;
+
+      cout << endl
+           << "Enter column #: ";
+      cin >> colInput;
+
+      while (spotTaken())
       {
-        play.win();
-        if (winner)
+        row = checkMove(rowInput, "row");
+        col = checkMove(colInput, "column");
+
+        // Reset variables
+        rowInput = -1;
+        colInput = -1;
+      }
+      makeMove(moves);
+
+      // No one can win until at least 5 moves are made
+      if (moves >= 4)
+      {
+        winner = checkWin(moves);
+        if (!winner.empty())
         {
-          break; //if winner found, end game
+          cout << winner + " wins!" << endl;
+
+          cout << endl
+               << "Play again? (Y/N): ";
+          cin >> playAgain;
+          break;
         }
       }
     }
-    cout << endl
-         << "Play again? (Y/N): "; //ask user to play again
-    cin >> again;
   }
-} //end program
+}
